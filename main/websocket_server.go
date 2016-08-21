0bc29a96aaa6ae3b0833ec0e4db11402d361e1fe
Tim Mathews <tmathews04@gmail.com>
2016-08-21 14:45:37 -0400

    Combine WebSockets code into one file

--- main/websocket_server.go
+++ main/websocket_server.go
@@ -20,9 +20,18 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/websocket"
 	"github.com/op/go-logging"
 	"net/http"
+	"time"
 )
+
+type connection struct {
+	ws *websocket.Conn
+
+	send chan []byte
+}
 
 type hub struct {
 	// Known connections
@@ -38,11 +47,55 @@ type hub struct {
 	unregister chan *connection
 }
 
+const (
+	// Time allowed to write a message to a receiver
+	writeWait = 10 * time.Second
+
+	// Time allowed to read pong from a receiver
+	pongWait = 60 * time.Second
+
+	// Send pings to receivers
+	pingPeriod = 56 * time.Second
+
+	// Max message size
+	maxMessageSize = 512
+)
+
 var websocket_hub = hub{
 	broadcast:   make(chan []byte),
 	register:    make(chan *connection),
 	unregister:  make(chan *connection),
 	connections: make(map[*connection]bool),
+}
+
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
+
+func (c *connection) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
 }
 
 func (h *hub) run() {
@@ -66,9 +119,35 @@ func (h *hub) run() {
 	}
 }
 
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	// TODO: Add origin check / CORS support
+
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		log.Error("%v", err)
+		return
+	}
+
+	if r.URL.Path == "/ws/v1/data" {
+		c := &connection{send: make(chan []byte, 256), ws: ws}
+		websocket_hub.register <- c
+		go c.writePump()
+	} else if r.URL.Path == "/ws/v1/control" {
+		fmt.Println("Got registration")
+	}
+}
+
 func loggingHandler(handler http.Handler, log *logging.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Info("%v %v %v", r.RemoteAddr, r.Method, r.URL)
+		log.Infof("%v %v %v", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
 }
